2c9dd2585340b986231a6b3344d6328def13673d
williamchan <will@paxata.com>
2015-10-14 16:29:40 -0700

    With different certificates for server and client

--- webapp/src/test/create-certs.sh
+++ webapp/src/test/create-certs.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
  
-# default password for keys
-PASSWORD=password
- 
+CA_PASSWORD=6pSC5p8vf0nDv%*j
+SERVER_PASSWORD=g7^w@IzOgkPdBZjK
+CLIENT_PASSWORD=4wBtxVCpDSL@4PR8
+
 OUT_DIR=certs
- 
+
 # Subject items
 C="US"
 ST="CA"
 L="Redwood City"
-O="Paxata"
+O="PaxataDev"
  
-CN_CA="My CA Root"
+CN_CA="My CA Root 3"
 # This needs to be localhost if testing on https://localhost
 CN_SERVER="localhost"
 CN_CLIENT="Paxata User userA"
@@ -24,53 +25,52 @@ mkdir -p ${OUT_DIR}
 ###############################
  
 # create CA key
-openssl genrsa -des3 -out ${OUT_DIR}/ca.key -passout pass:${PASSWORD} 4096
+openssl genrsa -des3 -out ${OUT_DIR}/ca.key -passout pass:${CA_PASSWORD} 4096
  
 # create CA cert
 openssl req -new -x509 -days 365 -key ${OUT_DIR}/ca.key -out ${OUT_DIR}/ca.crt \
- -passin pass:${PASSWORD} -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/CN=${CN_CA}/"
+ -passin pass:${CA_PASSWORD} -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/CN=${CN_CA}/"
 
 # create truststore
 keytool -import -trustcacerts -alias caroot -file ${OUT_DIR}/ca.crt \
- -keystore ${OUT_DIR}/truststore.jks -storepass ${PASSWORD} -noprompt
+ -keystore ${OUT_DIR}/truststore.jks -storepass ${CA_PASSWORD} -noprompt
 
 ###############################
 
 # create server key
-openssl genrsa -des3 -out ${OUT_DIR}/server.key -passout pass:${PASSWORD} 4096
+openssl genrsa -des3 -out ${OUT_DIR}/server.key -passout pass:${SERVER_PASSWORD} 4096
 
 # create server cert request
 openssl req -new -key ${OUT_DIR}/server.key -out ${OUT_DIR}/server.csr \
- -passin pass:${PASSWORD} -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/CN=${CN_SERVER}/"
+ -passin pass:${SERVER_PASSWORD} -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/CN=${CN_SERVER}/"
 
 # create server cert
 openssl x509 -req -days 365 -in ${OUT_DIR}/server.csr -CA ${OUT_DIR}/ca.crt \
  -CAkey ${OUT_DIR}/ca.key -set_serial 01 -out ${OUT_DIR}/server.crt \
- -passin pass:${PASSWORD}
+ -passin pass:${CA_PASSWORD}
 
 # convert server cert to PKCS12 format, including key
 openssl pkcs12 -export -out ${OUT_DIR}/server.p12 -inkey ${OUT_DIR}/server.key \
- -in ${OUT_DIR}/server.crt -passin pass:${PASSWORD} -passout pass:${PASSWORD}
+ -in ${OUT_DIR}/server.crt -passin pass:${SERVER_PASSWORD} -passout pass:${SERVER_PASSWORD}
 
 keytool -v -importkeystore -srckeystore ${OUT_DIR}/server.p12 -srcstoretype PKCS12 \
  -destkeystore ${OUT_DIR}/keystore.jks -deststoretype JKS \
- -srcstorepass ${PASSWORD} -deststorepass ${PASSWORD}
+ -srcstorepass ${SERVER_PASSWORD} -deststorepass ${SERVER_PASSWORD}
 
 ###############################
- 
+
 # create client key
-openssl genrsa -des3 -out ${OUT_DIR}/client.key -passout pass:${PASSWORD} 4096
- 
+openssl genrsa -des3 -out ${OUT_DIR}/client.key -passout pass:${CLIENT_PASSWORD} 4096
+
 # create client cert request
 openssl req -new -key ${OUT_DIR}/client.key -out ${OUT_DIR}/client.csr \
- -passin pass:${PASSWORD} -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/CN=${CN_CLIENT}/"
- 
- 
+ -passin pass:${CLIENT_PASSWORD} -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/CN=${CN_CLIENT}/"
+
 # create client cert
 openssl x509 -req -days 365 -in ${OUT_DIR}/client.csr -CA ${OUT_DIR}/ca.crt \
  -CAkey ${OUT_DIR}/ca.key -set_serial 02 -out ${OUT_DIR}/client.crt \
- -passin pass:${PASSWORD}
- 
+ -passin pass:${CA_PASSWORD}
+
 # convert client cert to PKCS12, including key
 openssl pkcs12 -export -out ${OUT_DIR}/client.p12 -inkey ${OUT_DIR}/client.key \
- -in ${OUT_DIR}/client.crt -passin pass:${PASSWORD} -passout pass:${PASSWORD}
+ -in ${OUT_DIR}/client.crt -passin pass:${CLIENT_PASSWORD} -passout pass:${CLIENT_PASSWORD}
